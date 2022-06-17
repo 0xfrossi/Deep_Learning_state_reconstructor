9981660153aeb1b42e51935cd3e3ffbd36016702
lucalavazza <l.lavazza@studenti.unibs.it>
2022-06-18 00:56:13 +0200

    commit 3

--- main.py
+++ main.py
@@ -1,133 +1,136 @@
+import random
+import tensorflow as tf
+
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.models import Model
+
 from utils_functions import *
-from plan import *
-from collections import Counter
+
+"""
+Caricamento dei dizionari e dei piani
+"""
 
 
 dizionario_stati = load_file("./dizionario_stati")
 piani_caricati = load_file("./plans")
 
 
-def costruisci_vettore(dizionario, numero_piano):
-    lista_stati = piani_caricati[numero_piano].states
+"""
+Definizione dei metodi di costruzione dei vettori da dare in pasto all'autoencoder
+"""
+
+
+def costruisci_vettore(dizionario, lista_stati):
     lunghezza_dizionario = len(dizionario)
     vettore_stati = []
     for stato in lista_stati:
-        vettore = [0] * lunghezza_dizionario    # per ciascuno stato crea un vettore di zeri lungo come il dizionario
+        vettore = [0] * np.array([0]*lunghezza_dizionario, dtype=np.int8)
         for s in stato:
             for key in dizionario.keys():
                 if key == s:
                     vettore[dizionario[key] - 1] = 1
-        vettore_stati.append(vettore)
+                    break
+        t = tf.convert_to_tensor(vettore, dtype=tf.int8)
+        vettore_stati.append(t)
+    r = tf.convert_to_tensor(vettore_stati, dtype=None)
     return vettore_stati
 
 
-def costruisci_vettore_iniziale(dizionario, numero_piano):
-    stato_iniziale = piani_caricati[numero_piano].initial_state
-    lunghezza_dizionario = len(dizionario)
-    vettore_iniziale = [0] * lunghezza_dizionario
-    for s in stato_iniziale:
-        for key in dizionario:
-            if key == s:
-                vettore_iniziale[dizionario[key] - 1] = 1
-    return vettore_iniziale
-
-
-def costruisci_vettore_goal(dizionario, numero_piano):
-    stato_goal = piani_caricati[numero_piano].goals
-    lunghezza_dizionario = len(dizionario)
-    vettore_goal = [0] * lunghezza_dizionario
-    for s in stato_goal:
-        for key in dizionario:
-            if key == s:
-                vettore_goal[dizionario[key] - 1] = 1
-    return vettore_goal
-
-
-def stampa_info_piano(numero_piano):
-    print("***")
-    print("")
-    print("Piano numero:", numero_piano)
-    print("")
-    print("***")
-    print("")
-    print("Stato iniziale del piano", numero_piano, "->")
-    print(piani_caricati[numero_piano].initial_state)
-    print("Lunghezza vettore stato iniziale =", len(piani_caricati[numero_piano].initial_state))
-    print("")
-    print("***")
-    print("")
-    print("Stati intermedi del piano", numero_piano, "->")
-    i = 1
-    for p in piani_caricati[numero_piano].states:
-        print(i, ": ", p)
-        print("lunghezza vettore", i, "=", len(p))
-        i += 1
-    print("")
-    print("***")
-    print("")
-    print("Stato goal del piano", numero_piano, "->")
-    print(piani_caricati[numero_piano].goals)
-    print("Lunghezza vettore stato goal =", len(piani_caricati[1].goals))
-    print("")
-    print("***")
-
-
-def stampa_info_vettore_stati(vettore, numero_piano):
-    print("")
-    print("***")
-    print("")
-    print("Lunghezza del vettore degli stati:", len(vettore))
-    print("")
-    print("***")
-    print("")
-    for v in vettore:
-        print(v)
-        counts = Counter(v)
-        print(counts[1], "uni su", len(v), "zeri")
-
-
-def stampa_info_vettore_iniziale(vettore, numero_piano):
-    print("")
-    print("***")
-    print("")
-    print("Lunghezza del vettore iniziale:", len(vettore))
-    print("")
-    print("***")
-    print("")
-    print(vettore)
-    counts = Counter(vettore)
-    print(counts[1], "uni su", len(vettore), "zeri")
-
-
-def stampa_info_vettore_goal(vettore, numero_piano):
-    print("")
-    print("***")
-    print("")
-    print("Lunghezza del vettore goal:", len(vettore))
-    print("")
-    print("***")
-    print("")
-    print(vettore)
-    counts = Counter(vettore)
-    print(counts[1], "uni su", len(vettore), "zeri")
-
-
-# Non utilizzare adesso, va ottimizzata build_vector prima, sia a livello di complessità,
-# sia a livello di codifica di "1" e "0", e strttura dati da usare
-def build_all_vectors(dict, plans_list):
+# NON UTILIZZATO ADESSO
+# shape di ogni singolo elemento (r) è (n x 340) con n che varia su ogni piano, raggruppati sulla base dei piani
+def costruisci_tutti_vettori(dizionario, lista_piani):
     total = []
-    for plan in plans_list:
-        r = costruisci_vettore(dict, plan.states)
+    for piano in lista_piani:
+        r = costruisci_vettore(dizionario, piano.states)
         total.append(r)
+    # r = tf.convert_to_tensor(total,dtype=None)
     return total
 
 
-stampa_info_piano(1)
+# Gli stati vengono ordinati (in una lista) singolarmente con shape (1x340), non vengono raggruppati sulla base dei piani
+# Da utilizzare per autoencoder standard
+def costruisci_tutti_vettori_1x340(dizionario, lista_piani):
+    lunghezza_dizionario = len(dizionario)
+    total = []
+    for plan in lista_piani:
+        for stato in plan.states:
+            vettore = np.array([0]*lunghezza_dizionario, dtype=np.int8)
+            for s in stato:
+                for key in dizionario.keys():
+                    if key == s:
+                        vettore[dizionario[key]-1] = 1
+                        break
+            t = tf.convert_to_tensor(vettore, dtype=tf.int8)
+            total.append(t)
+    return total
 
-vettore_prova_stati = costruisci_vettore(dizionario_stati, 1)
-vettore_prova_iniziale = costruisci_vettore_iniziale(dizionario_stati, 1)
-vettore_prova_goal = costruisci_vettore_goal(dizionario_stati, 1)
 
-stampa_info_vettore_stati(vettore_prova_stati, 1)
-stampa_info_vettore_iniziale(vettore_prova_iniziale, 1)
-stampa_info_vettore_goal(vettore_prova_goal, 1)
+"""
+Preparazione dei dati
+"""
+
+
+def crea_set(dizionario, piani):
+    s_training = load_file("./set_training")
+    s_test = load_file("./set_test")
+    s_validation = load_file("./set_validation")
+
+    # se per qualche motivo almeno uno manca, li rifaccio tutti
+    if s_training is None or s_test is None or s_validation is None:
+        # Costruzione del dataset completo -> lo faccio solo se serve, non se ho già i file da caricare
+        tutti_1x340 = costruisci_tutti_vettori_1x340(dizionario, piani)
+        random.shuffle(tutti_1x340)
+        # Suddivisione in Train, Test e Validation sets. Input shape per la rete = 340
+        split = int(len(tutti_1x340)//3)
+        train_set = tutti_1x340[split:]
+        vt = tutti_1x340[:split]
+        sub_split = int(len(vt)//2)
+        validation_set = vt[sub_split:]
+        test_set = vt[:sub_split]
+        save_file(train_set, ".", "set_training")
+        save_file(validation_set, ".", "set_validation")
+        save_file(test_set, ".", "set_test")
+
+
+crea_set(dizionario_stati, piani_caricati)
+
+s_training = load_file("./set_training")
+s_test = load_file("./set_test")
+s_validation = load_file("./set_validation")
+
+"""
+Modello
+"""
+
+# Costante
+input_size = 340
+# Scelta casuale -> fare dei test
+hidden_size = 85
+code_size = 20
+
+
+my_callbacks = [
+    tf.keras.callbacks.EarlyStopping(patience=10),
+    tf.keras.callbacks.TensorBoard(log_dir="./logsTf")
+]
+
+
+# Cose da fare per migliorare la rete solo dopo aver fatto i primi tentativi con la rete proposta:
+# * Provare ad aggiungere Regolarizzazione es. L1,L2 e dropout(solo nella fase di encoding)
+# * Provare swish al posto di relu
+# * Provare keras.layers.BatchNormalization()
+# * Fare Hyperparameter Tuning  (ultima) www.tensorflow.org/tensorboard/hyperparameter_tuning_with_hparams
+# Salvare i risultati con ogni modifica fatta per scriverli nel report
+
+input_layer = Input(shape=(input_size,))
+hidden_1 = Dense(hidden_size, activation='relu', kernel_initializer="he_uniform")(input_layer)
+code = Dense(code_size, activation='relu', kernel_initializer="he_uniform")(hidden_1)
+hidden_2 = Dense(hidden_size, activation='relu', kernel_initializer="he_uniform")(code)
+output_layer = Dense(input_size, activation='sigmoid')(hidden_2)
+
+autoencoder = Model(input_layer, output_layer)
+# variare learning_rate beta_1,beta_2, da fare per ultimo
+# batch_size ???
+autoencoder.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.01), loss='binary_crossentropy', metrics=["accuracy", "Precision", "Recall"])
+autoencoder.fit(x=s_training, y=s_training, epochs=50, batch_size=10000, validation_data=(s_validation, s_validation), callbacks=my_callbacks, verbose=1)
+
